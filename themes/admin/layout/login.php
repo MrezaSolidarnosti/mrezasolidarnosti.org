a323f31b9d9ccdd8d4043a42f3196e3d1c827956
zip01 <petariv9595@gmail.com>
2026-08-26 17:11:12 +0200

    point favicon to the correct path in admin

--- themes/admin/layout/login.php
+++ themes/admin/layout/login.php
@@ -10,6 +10,8 @@
     <link rel="stylesheet" href="<?=ADMIN_ASSET_URL . '/css/style.css'?>">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="icon" href="/favicon.ico" sizes="any">
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100;0,9..40,200;0,9..40,300;0,9..40,400;0,9..40,500;0,9..40,600;0,9..40,700;0,9..40,800;0,9..40,900;0,9..40,1000;1,9..40,100;1,9..40,200;1,9..40,300;1,9..40,400;1,9..40,500;1,9..40,600;1,9..40,700;1,9..40,800;1,9..40,900;1,9..40,1000&display=swap" rel="stylesheet">
 </head>
 <body>
